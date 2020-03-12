7b9781b99f6781d9b78a9feddf4fe2c7f945c0c9
Shraddha Kasaudhan <39942928+Shraddha1109@users.noreply.github.com>
2020-03-12 12:01:04 +0530

    Update steamer.py

--- steamer.py
+++ steamer.py
@@ -1,19 +1,23 @@
 from flask_opencv_streamer.streamer import Streamer
 import cv2
 
-port = 3030
-require_login = False
-streamer = Streamer(port, require_login)
+class VideoCamera(object):
+    def __init__(self):
+        # Using OpenCV to capture from device 0. If you have trouble capturing
+        # from a webcam, comment the line below out and use a video file
+        # instead.
+        self.video = cv2.VideoCapture(0)
+        # If you decide to use video.mp4, you must have this file in the folder
+        # as the main.py.
+        # self.video = cv2.VideoCapture('video.mp4')
 
-# Open video device 0
-video_capture = cv2.VideoCapture(0)
+    def __del__(self):
+        self.video.release()
 
-while True:
-    _, frame = video_capture.read()
-
-    streamer.update_frame(frame)
-
-    if not streamer.is_streaming:
-        streamer.start_streaming()
-
-    cv2.waitKey(30)
+    def get_frame(self):
+        success, image = self.video.read()
+        # We are using Motion JPEG, but OpenCV defaults to capture raw images,
+        # so we must encode it into JPEG in order to correctly display the
+        # video stream.
+        ret, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
